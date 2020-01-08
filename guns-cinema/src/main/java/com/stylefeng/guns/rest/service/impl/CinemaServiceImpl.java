2d72c5b41787170cf0a627fb4938f1885c43d46d
WhiteDragon96 <cctvsxu.163.com>
2020-01-08 19:53:26 +0800

    未完成

--- guns-cinema/src/main/java/com/stylefeng/guns/rest/service/impl/CinemaServiceImpl.java
+++ guns-cinema/src/main/java/com/stylefeng/guns/rest/service/impl/CinemaServiceImpl.java
@@ -3,11 +3,27 @@ package com.stylefeng.guns.rest.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.stylefeng.guns.rest.cinema.CinemaService;
+import com.stylefeng.guns.rest.common.persistence.dao.MtimeCinemaTMapper;
+import com.stylefeng.guns.rest.common.persistence.model.MtimeCinemaT;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 @Service(interfaceClass = CinemaService.class)
 public class CinemaServiceImpl implements CinemaService {
+    @Autowired
+    MtimeCinemaTMapper mtimeCinemaTMapper;
+    /**
+     * 获取播放场次
+     * @param cinemaId
+     * @return
+     */
+    @Override
+    public Map getFields(Integer cinemaId) {
+        MtimeCinemaT mtimeCinemaT = mtimeCinemaTMapper.selectById(cinemaId);
 
-
+        return null;
+    }
 }
