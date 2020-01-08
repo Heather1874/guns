df250de1bc7c1120b923516e4957c8ffa7458124
twotut00 <2814874096@qq.com>
2020-01-08 17:48:45 +0800

    明天吃肉

--- guns-api/src/main/java/com/stylefeng/guns/rest/common/BaseResqVo.java
+++ guns-api/src/main/java/com/stylefeng/guns/rest/common/BaseResqVo.java
@@ -2,8 +2,10 @@ package com.stylefeng.guns.rest.common;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class BaseResqVo<T> {
+public class BaseResqVo<T> implements Serializable {
     T data;
     String imgPre = "http://img.meetingshop.cn/";
     Integer status = 0;
--- guns-gateway/src/main/java/com/stylefeng/guns/rest/cinemacontroller/CinemaController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/cinemacontroller/CinemaController.java
@@ -1,9 +1,19 @@
 package com.stylefeng.guns.rest.cinemacontroller;
 
+import com.stylefeng.guns.rest.common.CinemaReqsVo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class CinemaController {
 
+    @RequestMapping("cinema/getCinemas")
+    public CinemaReqsVo getCinemas(@RequestParam Map map){
+        CinemaReqsVo cinemaReqsVo = new CinemaReqsVo();
+        return cinemaReqsVo;
+    }
 
 }
